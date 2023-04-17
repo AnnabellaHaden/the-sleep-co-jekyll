2f1e6fb19908d45b5babd4241aa88407162e35e5
AnnabellaHaden <127132441+AnnabellaHaden@users.noreply.github.com>
2023-04-17 18:02:38 +0100

    If Statement

--- scripts.js
+++ scripts.js
@@ -4,3 +4,13 @@ $(document).ready(function() {
         $(".mobile-menu").fadeToggle()
     });
 });
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
